7b51921e909f6412e2876a5059f17bfe119cea5a
Vishal15276 <vishal15276@gmail.com>
2025-03-21 22:24:34 +0530

    Update deploy.sh

--- Task 2/deploy.sh
+++ Task 2/deploy.sh
@@ -2,7 +2,7 @@
     #echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u vishal15276t -p abccccc
+    docker login -u vishal15276t -p
     docker tag test1 vishal15276t/test1
     docker push vishal15276t/test1
     docker compose up -d
